aef4c070a4e5b9c5f74273ed98db71af339dae12
zengww <justzww@gmail.com>
2019-03-25 10:29:19 +0800

    update pipenv

--- handler.py
+++ handler.py
@@ -3,23 +3,23 @@
 
 import logging
 from datetime import datetime
-from socket import gethostname
 from pymongo import MongoClient
 from bson import InvalidDocument
 
 
 class MongoFormatter(logging.Formatter):
+
     def format(self, record):
         """制定需要的日志信息"""
         data = record.__dict__.copy()
 
         if record.args:
             record.msg = record.msg % record.args
-		# 添加需要的信息
+        # 添加需要的信息
         data.update(
             time=datetime.now(),
             message=record.msg,
-            args=tuple(unicode(arg) for arg in record.args)
+            args=tuple(arg for arg in record.args)
         )
         if 'exc_info' in data and data['exc_info']:
             data['exc_info'] = self.formatException(data['exc_info'])
@@ -27,8 +27,8 @@ class MongoFormatter(logging.Formatter):
 
 
 class MongoHandler(logging.Handler):
-    """ 
-	控制类
+    """
+        控制类
     """
 
     @classmethod
@@ -49,6 +49,5 @@ class MongoHandler(logging.Handler):
         """ 往mongodb存储日志信息"""
         try:
             self.collection.save(self.format(record))
-        except InvalidDocument, e:
+        except InvalidDocument as e:
             logging.error("Unable to save log record: %s", e.message, exc_info=True)
-
